40e023b6f4756a9021ac60a8baab41e20c7d56f2
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-08-12 23:01:19 -0300

    Tabuada do valor escolhido

---
+++ FirstCodes/src/Exercicios/Tabuada.java
@@ -0,0 +1,22 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+        Scanner escolha = new Scanner(System.in);
+        int valorFinal = 0;
+        System.out.println("Escolha um numero para calcular a tabuada: ");
+        int calculos = escolha.nextInt();
+
+        for (int tabuada = 0; tabuada <= 10; tabuada++) {
+            valorFinal = calculos * tabuada;
+            System.out.printf("%d * %d = %d \n", calculos, tabuada, valorFinal);
+        }
+
+
+
+        escolha.close();
+    }
+}
